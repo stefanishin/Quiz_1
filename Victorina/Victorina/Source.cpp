9b2b87c9ff963cae5193679977aab1f60ca331c1
Stefanishin Mikita <stefanishin92@gmail.com>
2020-10-21 15:51:51 +0300

    test 8 error

--- Victorina/Victorina/Source.cpp
+++ Victorina/Victorina/Source.cpp
@@ -2,20 +2,28 @@
 #include<conio.h>
 #include<cstdlib>
 using namespace std;
+//8 error 3
+class A {
+public:
+    float v;
+    float set(float v) {
+        A::v += 1.0;
+        return v;
+    }
+    float set(void) {
+        A::v = v + 1.0;
+        return 0.0;
+    }
+    float get(float v) {
+        v += A::v;
+        return v;
+    }
+};
 
-int main(void)
-{
-	int year;
-	cout << "Enter a year:";
-	cin >> year;
-	if(year>2000)
-	{
-		cout << "Common year";
-	}
-	else
-	{
-		cout << "no";
-	}
+int main() {
+    A a;
+    cout << a.get(a.set(a.set(a.set())));
+
 
 	system("pause");
 return 0;
